395dd5f55d2ebd5e454b53391d091e717a82ac18
SHIVAM GUPTA <shivamg11000@gmail.com>
2017-11-07 09:52:42 +0530

    added static files in public folder

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs') // set view engine
 
+app.use('/public', (req, res) => {
+    console.log(req.url)
+    res.sendFile(`${__dirname}/public${req.url}`)
+})
+//app.use('/public', express.static(path.join(__dirname, 'public')))
+
+
 app.get(['/', '/home'], (req, res) => { // render looks for file in views
     res.render('index.ejs')
 })
 app.get('/contact', (req, res) => {
-    res.render('contact.ejs')
+    res.render('contact.ejs', {qs: req.query})
 })
 
 app.get('/profile/:name', (req, res) => {
@@ -15,8 +23,8 @@ app.get('/profile/:name', (req, res) => {
     res.render('profile', {person: req.params.name, data})  // passing data to the view profile
 })
 
-app.get('*', (req, res) => {  // for all other url paths
-    res.send('<h1>404</h1>')
-})
+//app.get('*', (req, res) => {  // for all other url paths
+//    res.send('<h1>404</h1>')
+//})
 
-app.listen(process.env.PORT || 8000, console.log('Website running on http://localhost:8000') )
+app.listen(process.env.PORT || 8001, console.log('Website running on http://localhost:8000') )
